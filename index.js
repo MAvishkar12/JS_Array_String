7165bbfc485d60b83ee4225e17fdca1feeacaaca
MAvishkar12 <moreavishkar12@gamil.com>
2025-03-04 20:52:40 +0530

    New array problems add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function isSubset(arr1,arr2){
-    return arr2.every(val=>arr1.includes(val))
+function FlattenArray( arr){
+    return arr.flat(Infinity)
 }
-const arr1=[1,2,3,4,5,6,7,8]
-const arr2=[3,4,5]
-console.log(isSubset(arr1,arr2));
+
+let arr = [1, 2, [3, 4, [5, [6, 7, [8]]]]];
+console.log(FlattenArray(arr))
+
